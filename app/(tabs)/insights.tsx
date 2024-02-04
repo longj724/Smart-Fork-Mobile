1f49c33b06ead1c51d5223fc0044b3c3fa4eee21
longj724 <justinlong596@gmail.com>
2024-02-03 21:36:01 -0500

    Utils and insights page test

--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -1,11 +1,46 @@
-import { View, Text } from "react-native";
+// External Dependencies
+import { View, Text, Pressable } from "react-native";
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
+import { useAuth } from "@clerk/clerk-expo";
 
-const insights = () => {
+const Page = () => {
+  const { getToken, userId } = useAuth();
+
+  const { mutate: submitQuestion } = useMutation({
+    mutationFn: async (question: string) => {
+      const supabaseAccessToken = await getToken({
+        template: "supabase",
+      });
+
+      const data = {
+        question,
+        userId,
+      };
+
+      return axios.post("http://localhost:3000/insights/ask-question", data, {
+        headers: {
+          Authorization: supabaseAccessToken,
+        },
+      });
+    },
+    mutationKey: ["askQuestion"],
+  });
+
   return (
     <View>
-      <Text>Insights</Text>
+      <Pressable
+        className="w-20 h-10 bg-green-700"
+        onPress={() =>
+          submitQuestion(
+            "What have I eaten over the last few days and what should I look to add to my diet?"
+          )
+        }
+      >
+        <Text>Submit Question Test</Text>
+      </Pressable>
     </View>
   );
 };
 
-export default insights;
+export default Page;
