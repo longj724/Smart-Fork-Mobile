d72be6784f2d4f9582bfca91ff3402c0788b3814
longj724 <justinlong596@gmail.com>
2024-02-25 22:51:55 -0500

    Sign out functionality

--- app/(tabs)/log/index.tsx
+++ app/(tabs)/log/index.tsx
@@ -1,5 +1,5 @@
 // External Dependencies
-import { Feather, Ionicons } from '@expo/vector-icons';
+import { Entypo, Feather, Ionicons } from '@expo/vector-icons';
 import {
   Modal,
   Pressable,
@@ -23,11 +23,12 @@ import { useAllMealsQuery } from '@/hooks/useAllMealsQuery';
 import ConnectWorkout from '@/components/ConnectWorkout';
 
 const Page = () => {
-  const { userId } = useAuth();
+  const { userId, signOut } = useAuth();
   const router = useRouter();
 
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [viewExtensionsMenu, setViewExtensionsMenu] = useState(false);
   const prevSelectedDateRef = useRef(selectedDate);
 
   const { data, refetch } = useAllMealsQuery(userId, selectedDate);
@@ -70,6 +71,11 @@ const Page = () => {
     });
   };
 
+  const onSignOut = () => {
+    signOut();
+    router.push('/login');
+  };
+
   const displayMeals = () => {
     const filteredData = data
       ?.filter(
@@ -103,39 +109,64 @@ const Page = () => {
 
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <View className="flex h-16 flex-row items-center justify-between bg-white">
+      <View className="flex h-16 flex-row items-center justify-evenly bg-green-700 z-10">
         <Feather
           name="mic"
           size={24}
-          style={{ marginLeft: 20 }}
+          style={{ marginRight: 50 }}
           onPress={quickAdd}
-          color="#15803D"
+          color="white"
         />
         <View className="flex flex-row justify-center gap-2">
           <Pressable
-            className="h-8 w-8 items-center justify-center rounded-sm bg-green-700"
+            className="h-8 w-8 items-center justify-center rounded-sm bg-white"
             onPress={moveBackwardOneDay}
           >
-            <Ionicons name="arrow-back-outline" color="white" size={16} />
+            <Ionicons name="arrow-back-outline" color="black" size={16} />
           </Pressable>
           <Pressable
-            className="flex w-28 flex-row items-center justify-center gap-1 rounded-sm bg-green-700"
+            className="flex w-28 flex-row items-center justify-center gap-1 rounded-sm bg-white"
             onPress={toggleDatePicker}
           >
-            <Text className="font-semibold text-white">
+            <Text className="font-semibold text-black">
               {displayDayInHeader()}
             </Text>
-            <Ionicons name="caret-down-outline" color="white" />
+            <Ionicons name="caret-down-outline" color="black" />
           </Pressable>
           <Pressable
-            className="h-8 w-8 items-center justify-center rounded-sm bg-green-700"
+            className="h-8 w-8 items-center justify-center rounded-sm bg-white"
             onPress={moveForwardOneDay}
           >
-            <Ionicons name="arrow-forward-outline" color="white" size={16} />
+            <Ionicons name="arrow-forward-outline" color="black" size={16} />
           </Pressable>
         </View>
-        {/* Empty View to balance the layout */}
-        <View style={{ width: 24, marginLeft: 20, opacity: 0 }}></View>
+        <View className="relative">
+          <Entypo
+            name="dots-three-horizontal"
+            size={24}
+            style={{ marginLeft: 50 }}
+            color="white"
+            onPress={() => setViewExtensionsMenu(!viewExtensionsMenu)}
+          />
+          {viewExtensionsMenu && (
+            <View
+              className="absolute right-0 mt-8 w-36 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="menu-button"
+            >
+              <Pressable className="py-1" role="none" onPress={onSignOut}>
+                <Text
+                  className="text-gray-800 block px-4 py-2 text-sm"
+                  role="menuitem"
+                  id="menu-item-0"
+                >
+                  Sign Out
+                </Text>
+              </Pressable>
+            </View>
+          )}
+        </View>
       </View>
 
       {showDatePicker && (
